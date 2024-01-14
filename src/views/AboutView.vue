9d38587dcd27c4fbc2350d4aa260b69dbf3b0755
HaiGood <sjh17379500517@outlook.com>
2024-01-14 22:11:16 +0800

    no message

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <h1>这里是模板内容：<span v-bind:id="id1">{{count}}</span>次点击</h1>
     <button v-on:click="clickButton">按钮</button>
+    <h1>新增内容!</h1>
   </div>
 </template>
 <script>
