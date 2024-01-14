c853357440038f057c7296cd6376e3259b5c8a3d
HaiGood <sjh17379500517@outlook.com>
2024-01-14 21:48:15 +0800

    no message

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>这里是模板内容：<span v-bind:id="id1">{{count}}</span>次点击</h1>
+    <button v-on:click="clickButton">按钮</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      id1: "h1"
+    }
+  },
+  methods: {
+    clickButton() {
+      this.count++;
+    }
+  }
+}
+</script>
